4af223de58a3fc8e59875d9dbd86930dce4d89f3
Paweł Boroń <pawcio2001.boron@interia.pl>
2025-01-06 22:56:32 +0100

    Dodanie tailwinda, dodanie loga

--- front-end-task/src/App.tsx
+++ front-end-task/src/App.tsx
@@ -4,14 +4,18 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { ParticipantsList } from "./ParticipantsList";
 import { EditParticipant } from "./EditParticipant";
 
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<ParticipantsList />} />
-          <Route path="/participants/:id" element={<EditParticipant />} />
-        </Routes>
+      <div className="App p-10 flex flex-col gap-10">
+        <img src="/img/logo.svg" className="w-20 h-20" alt="Logo Forte Digital"></img>
+        <div className="bg-[#F7F7F7] px-[68px] pt-[40px] pb-[80px] w-fit self-center">
+          <Routes>
+            <Route path="/" element={<ParticipantsList />} />
+            <Route path="/participants/:id" element={<EditParticipant />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
